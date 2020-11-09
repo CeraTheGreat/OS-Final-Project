bc98b99a2dd188da09d2aee5e8357b8be71761f5
GabeAvacado <48485882+GabeAvacado@users.noreply.github.com>
2020-11-08 19:39:33 -0600

    Fixed customer in main.c
    Customer no longer waits as it should. Added a training_floor array to keep track of the training floor space availability. Trainer needs to be fixed. Comments above functions explaining what they should do. Not sure how to go about trainers being with a customer and checking waiting room upon releasing a customer. Also pthread cust[] should be the customers that are coming in, not the total amount of customers the gym can hold.

--- part1/main.c
+++ part1/main.c
@@ -22,24 +22,52 @@ uint16_t pop_queue();
 bool queue_has_slot();
 bool queue_has_value();
 
+int training_floor[MAX_TRAINERS]; //training floor
+int training = 0;	//keeps track of amt of trainers/customers on floor
+int done_training = 0; //dno what this will do
+
 //semaphores
 sem_t empty; //number of empty slots
 sem_t full; //number of slots filled
 pthread_mutex_t mutex;
 
-void *customer(void *cno){
-	sem_wait(&empty);	//wait when there are no couches
+//Customers come in and look at the training floor.
+//If there's an open spot on the floor, then the customer goes to the floor.
+//If there's not an open spot, then the customer looks at the waiting room.
+//If there's an open spot in the waiting room, then the customer goes to the waiting room.
+//If there's not an open spot, then the customer leaves.
+
+void *customer(void *cno){	
 	pthread_mutex_lock(&mutex);
-	push_queue(*((uint16_t *)cno));	//add customer to queue
-	printf("Customer %d is in the wait queue!\n", *((int *)cno));
+	if((training + 1) % (MAX_TRAINERS + 1) == 0) {	//if there's not room on the floor
+		if (!queue_has_slot()) {	//if there's no space in waiting room
+			printf("Customer %d left due to no space!\n",*((int *)cno));
+			pthread_mutex_unlock(&mutex);	//unlock before exiting thread
+			pthread_exit(cno);	//if no space in waiting room, leave (exit current thread)
+		}
+		else{	//There is space in waiting room
+			push_queue(*((uint16_t *)cno)); 	//put customer in queue (waiting room)
+			printf("Customer %d is in the wait queue!\n", *((int *)cno));
+		}
+	}
+	else {	//space available on training floor
+		training_floor[training] = *(int *)cno;	//Put customer onto workout floor
+		printf("Customer %d going to training floor!\n", *((int *)cno));
+		training = (training + 1)%(MAX_TRAINERS + 1);	//increase number of current customers on floor
+	}
 	pthread_mutex_unlock(&mutex);
 	sem_post(&full);	//add space to full semaphore
 }
 
+//If trainers are waiting, grab the next customer that comes in till all customers are training.
+//If a trainer is done training, they need to look at the waiting room.
+//If a customer is in the waiting room, grab the first customer that entered.
+//If a customer is not in the waiting room, busy wait till next customer comes.
+
 void *trainer(void *tno) {
 	sem_wait(&full);	//wait when no customers
 	pthread_mutex_lock(&mutex);
-	int customer_training = pop_queue();	//take customer off waitlist and start training
+	int customer_training = pop_queue();	//take customer off waitlist and start training IF COMING FROM TRAINING OTHER CUSTOMER
 	printf("Trainer %d is training cutomer %d\n", *((int *)tno), customer_training);
 	//do some stuff with customer and weights (part 2?)
 	
@@ -51,13 +79,13 @@ int main(int argc, char **argv) {
 	/* set wait slot value, IDK where we get this */
 	wait_queue.actual_wait_slots = 6;
 	
-	pthread_t train[MAX_TRAINERS], cust[MAX_WAIT_SLOTS];	//delcare the trainer and customer threads
+	pthread_t train[MAX_TRAINERS], cust[20];	//delcare the trainer and customer threads. Amt of customer threads should be amount of customers coming?
 	pthread_mutex_init(&mutex, NULL);			//initilize mutex
 	sem_init(&empty,0,wait_queue.actual_wait_slots);	//initilize empty semaphore
 	sem_init(&full,0,0);					//initilize full semaphore
 	
 	int a[] = {1,2,3,4,5}; //keeping track of trainers
-	int b[] = {1,2,3,4,5,6}; //keeping track of customers
+	int b[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; //keeping track of customers
 
 	/* add
 	printf("%d\n", push_queue(1));
@@ -82,7 +110,7 @@ int main(int argc, char **argv) {
 	//todo:if no trainer available, "walk" to waiting room at certain speed?
 	
 	//Creates 6 threads(customers) that enter waiting room
-	for(int i = 0; i < wait_queue.actual_wait_slots; i++) {
+	for(int i = 0; i < 20; i++) {
 		pthread_create(&cust[i], NULL, (void *)customer, (void *)&b[i]);
 	}
 	
@@ -90,17 +118,19 @@ int main(int argc, char **argv) {
 	//todo:after training, check queue once at certain speed?
 	
 	//Creates 5 trainers to train customers
-	for(int i = 0; i < MAX_TRAINERS; i++) {
-		pthread_create(&train[i], NULL, (void *)trainer, (void *)&a[i]);
-	}
+//	for(int i = 0; i < MAX_TRAINERS; i++) {
+//		pthread_create(&train[i], NULL, (void *)trainer, (void *)&a[i]);
+//	}
 	
 	//Don't let main exit before threads do their thing
-	for(int i = 0; i < 6; i++) {
+	for(int i = 0; i < 20; i++) {
 		pthread_join(cust[i], NULL);
 	}
+	/*
 	for(int i = 0; i < 5; i++) {
 		pthread_join(train[i], NULL);
 	}
+	*/
 	
 	//destory mutex and semaphores before exiting
 	pthread_mutex_destroy(&mutex);
