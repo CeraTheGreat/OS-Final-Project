602b032763428d48332b00c911a5c160db0a7dd1
Jennifer Cera Rotelli <csr0897@gmail.com>
2020-12-01 02:24:17 -0600

    big changes, shared memory implimented, ipc with pipes, semaphores the whole shebang

--- part1/main.c
+++ part1/main.c
@@ -4,6 +4,15 @@
 #include <semaphore.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <sys/prctl.h>
+#include <signal.h>
+
+#include "main.h"
+
+struct trainer_s trainer;
 
 int num_trainers = MAX_TRAINERS;
 int num_customers;
@@ -11,46 +20,75 @@ sem_t cus;
 sem_t train;
 sem_t mutex;
 
-void trainer(int num_customers);
-void customer(int num_customers);
+//void trainer(int num_customers);
+//void customer(int num_customers);
 
 int main(int argc, char **argv) {
-	/* set wait slot value, IDK where we get this */
-	wait_queue.actual_wait_slots = 6;
+	/* set up shared memory for processes */
+	key_t sharedm_key = ftok(PROJECT_PATH, PROJECT_ID);
+	int sharedm_id = shmget(
+			sharedm_key,
+			sizeof(struct sharedm_s),
+			0666 | IPC_CREAT);
 
-	/* add */
-	//printf("%d\n", push_queue(1));
-	//printf("%d\n", push_queue(2));
-	//printf("%d\n", push_queue(3));
-	//printf("%d\n", push_queue(4));
-	//printf("%d\n", push_queue(5));
-	//printf("%d\n", push_queue(6));
-	//printf("%d\n", push_queue(6));
+	/* attatch sharedmem to this process so we can set things up */
+	struct sharedm_s* main_share = (struct sharedm_s*) shmat(sharedm_id, (void*)0, 0);
 
+	/* initialize mutex in shared memory */
+	//pthread_mutex_init(&main_share->checking_mutex, NULL);
+	sem_init(&main_share->semaphore, 0, SEMAPHORE_INIT);
 
-	//printf("%d\n", pop_queue());
-	//printf("%d\n", pop_queue());
-	//printf("%d\n", pop_queue());
-	//printf("%d\n", pop_queue());
-	//printf("%d\n", pop_queue());
-	//printf("%d\n", pop_queue());
-	//printf("%d\n", pop_queue());
-	
-	//Need to do some testing. Print some stuff knowing if everything is working properly and to show.
-	for (int i = 0; i < 20; i++) { //When does this become false? Idk when program should end.
-		num_customers = 0;
-		customer(num_customers);
-		int pid = fork();	// check for error
-		trainer(num_customers);
+	/* set wait slot value in shared memory, IDK where we get this */
+	main_share->wait_queue.actual_wait_slots = 6;
+
+	/* create trainer processes, assigned id by i */
+	for (int i = 0; i < 20; i++) {
+		pid_t child_pid;
+
+		if ((child_pid = fork()) == 0) {
+			/* now we're in a trainer process - let's set things up */
+			/* kill child when parent dies */
+			prctl(PR_SET_PDEATHSIG, SIGHUP);
+
+			trainer.id = i;
+
+			/* get the shared mem info */
+			key_t key = ftok(PROJECT_PATH, PROJECT_ID);
+			int shmid = shmget( 
+					key, 
+					sizeof(struct sharedm_s), 
+					0666 | IPC_CREAT);
+
+			/* attatch to shared memory */
+			trainer.share = (struct sharedm_s*) shmat(shmid, (void*)0, 0);
+
+			/* create pipes for trainer communication */
+			pipe(trainer.share->pipes[trainer.id].in);
+			pipe(trainer.share->pipes[trainer.id].out);
+
+			run_trainer();
+		}
 	}
+
+	// destroy shared memory stuff when we're done
+	shmctl(sharedm_id, IPC_RMID, NULL);
+	//pthread_mutex_destroy(&main_share->checking_mutex);
+
+	////Need to do some testing. Print some stuff knowing if everything is working properly and to show.
+	//for (int i = 0; i < 20; i++) { //When does this become false? Idk when program should end.
+	//	num_customers = 0;
+	//	customer(num_customers);
+	//	int pid = fork();	// check for error
+	//	trainer(num_customers);
+	//}
 
 	return 0;
 }
 
-uint16_t push_queue(uint16_t value) {
-	if (queue_has_slot()) {
-		wait_queue.slots[wait_queue.ins_pos++] = value;
-		wait_queue.ins_pos %= (wait_queue.actual_wait_slots + 1);
+uint16_t push_queue(uint16_t value, struct queue_s *queue) {
+	if (queue_has_slot(queue)) {
+		queue->slots[queue->ins_pos++] = value;
+		queue->ins_pos %= (queue->actual_wait_slots + 1);
 		return value;
 	} else {
 		/* return 0 on failure */
@@ -58,10 +96,10 @@ uint16_t push_queue(uint16_t value) {
 	}
 }
 
-uint16_t pop_queue() {
-	if (queue_has_value()) {
-		uint16_t value = wait_queue.slots[wait_queue.read_pos++];
-		wait_queue.read_pos %= (wait_queue.actual_wait_slots + 1);
+uint16_t pop_queue(struct queue_s *queue) {
+	if (queue_has_value(queue)) {
+		uint16_t value = queue->slots[queue->read_pos++];
+		queue->read_pos %= (queue->actual_wait_slots + 1);
 		return value;
 	} else {
 		/* return 0 on failure */
@@ -69,40 +107,69 @@ uint16_t pop_queue() {
 	}
 }
 
-bool queue_has_slot() {
-	return (!((wait_queue.ins_pos + 1) % (wait_queue.actual_wait_slots + 1) ==
-			wait_queue.read_pos));
+bool queue_has_slot(struct queue_s *queue) {
+	return (!((queue->ins_pos + 1) % (queue->actual_wait_slots + 1) ==
+			queue->read_pos));
 }
 
-bool queue_has_value() {
-	return (!(wait_queue.ins_pos == wait_queue.read_pos));
+bool queue_has_value(struct queue_s *queue) {
+	return (!(queue->ins_pos == queue->read_pos));
 }
 
-void trainer(int num_customers) {
-	sem_wait(&cus);
-	sem_wait(&mutex);
-	num_customers--;
-	if (num_trainers > 0) { // if num_trainers > 0 decrease amt of available trainers and set trainer available
-		num_trainers--;
-		sem_post(&train);
-	}
-	sem_post(&mutex);
-	//do_coaching();
-}
+void run_trainer() {
+	char buff[PIPE_BUFF_SIZE];
+	ssize_t chars_read;
+	while (true) {
+		/* set trainer as idle */
+		trainer.share->is_trainer_idle[trainer.id] = true;
+		/* blocking read, wait for customer*/
+		chars_read = read(trainer.share->pipes[trainer.id].in[READ], buff, PIPE_BUFF_SIZE);
 
-void customer(int num_customers) {
-	sem_wait(&mutex);
-	if(queue_has_slot()) {
-		num_customers++;
-		sem_post(&cus);
-		sem_post(&mutex);
-		sem_wait(&train);
-		if (num_trainers < 6) {
-			//do_training();
-			num_trainers++; //need another semaphore? to prevent context switching?
+		train_customer();
+
+		/* when training is complete, check for customers in the waiting room */
+		/* lock semaphore to exclude other searchers */
+		//sem_wait(&trainer.share->semaphore);
+		if (queue_has_value(&trainer.share->wait_queue)) {
+			/* TODO: pass trainer id to customer and they will join trainer */
 		}
-	}
-	else {
-		sem_post(&mutex);
+
+		/* release semaphore after we've done our search */
+		//sem_post(&trainer.share->semaphore);
 	}
 }
+
+void train_customer() {
+	/* read list, instruct user on what to do, wait for them to finish */
+	/* sleep for now, until it's implimented */
+	sleep(2);
+}
+
+//void trainer(int num_customers) {
+//	sem_wait(&cus);
+//	sem_wait(&mutex);
+//	num_customers--;
+//	if (num_trainers > 0) { // if num_trainers > 0 decrease amt of available trainers and set trainer available
+//		num_trainers--;
+//		sem_post(&train);
+//	}
+//	sem_post(&mutex);
+//	//do_coaching();
+//}
+//
+//void customer(int num_customers) {
+//	sem_wait(&mutex);
+//	if(queue_has_slot()) {
+//		num_customers++;
+//		sem_post(&cus);
+//		sem_post(&mutex);
+//		sem_wait(&train);
+//		if (num_trainers < 6) {
+//			//do_training();
+//			num_trainers++; //need another semaphore? to prevent context switching?
+//		}
+//	}
+//	else {
+//		sem_post(&mutex);
+//	}
+//}
--- part1/main.h
+++ part1/main.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <pthread.h>
 
 /* as specified by the assignment */
 #define MAX_TRAINERS 5
@@ -11,21 +12,58 @@
 /* maximum amount of time between customer spawns */
 #define MAX_USLEEP 30
 
+/* shared memory constants */
+#define PROJECT_PATH "share"
+#define PROJECT_ID 1
+
+#define SEMAPHORE_INIT 1
+
+#define PIPE_BUFF_SIZE 128
+#define WRITE 0
+#define READ 1
+
 /* circular queue, FIFO */
-struct _queue {
+struct queue_s {
 	size_t actual_wait_slots;
 	uint16_t slots[MAX_WAIT_SLOTS + 1];
 	size_t ins_pos;
 	size_t read_pos;
 };
 
-uint16_t push_queue(uint16_t value);
-uint16_t pop_queue();
-bool queue_has_slot();
-bool queue_has_value();
+/* for communication between trainer and customer */
+struct trainer_pipes {
+	int in[2];
+	int out[2];
+};
+
+struct waitroom_pipes {
+	int fd[2];
+};
+
+/* shared memory for ipc */
+struct sharedm_s {
+	sem_t semaphore;
+	//pthread_mutex_t checking_mutex;
+	struct queue_s wait_queue;
+	struct trainer_pipes pipes[MAX_TRAINERS];
+	bool is_trainer_idle[MAX_TRAINERS];
+};
+
+/* everything a single trainer needs to know */
+struct trainer_s {
+	struct sharedm_s *share;
+	uint8_t id;
+};
+
+uint16_t push_queue(uint16_t value, struct queue_s *queue);
+uint16_t pop_queue(struct queue_s *queue);
+bool queue_has_slot(struct queue_s *queue);
+bool queue_has_value(struct queue_s *queue);
 
 //void *customer_tf(void *argp);
 //void *trainer_tf(void *argp);
+void run_trainer();
+void train_customer();
 
-void trainer(int num_customers);
-void customer(int num_customers);
+//void trainer(int num_customers);
+//void customer(int num_customers);
